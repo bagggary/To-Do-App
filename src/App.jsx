7dbf5772d39af1c84048b8a16929b012e2af15e5
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-11-29 21:55:04 +0200

    created the basic function

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,43 @@ import React from 'react'
 import './App.css'
 import Header from './Components/Header'
 import Main from './Components/Main'
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [doList , setDoList] = React.useState([])
+  function keyHandler(e) {
+      if(e.target.value !== ''){
+          if(e.keyCode === 13){
+              const tasks = {
+                  id : nanoid(),
+                  task : e.target.value,
+                  completed: false
+              }
+              setDoList(prev => [...prev, tasks])
+              e.target.value = ''
+      }
+      }else{
+          return
+      }
+  }
+  function onChange(e){
+    doList.map((tasks) => {
+      if(e.target.id === tasks.id){
+        setDoList(prev => {
+          return{
+            ...prev,
+            completed : e.target.checked
+          }
+        })
+      } 
+    })
+  }
+  console.log(doList)
 
   return (
     <>
     <Header/>
-    <Main/>
+    <Main onChange= {onChange} doList = {doList} keyHandler= {keyHandler}/>
     </>
   )
 }
