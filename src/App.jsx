a5a5cc9fa9d6ed6d0971acce0f1b9643861484e9
ENG-SKYLER <ibraheema.a2014@gmail.com>
2022-12-10 23:33:53 +0200

    tasks stored on localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,25 @@ export const ThemeContext = React.createContext(null)
 
 function App() {
   const [doList , setDoList] = React.useState([])
-  const [filteredTasks , setFilteredTasks] = React.useState(tasksFiltered())
-  const [count , setCount] = React.useState(0)
+  const [filteredTasks , setFilteredTasks] = React.useState([])
   const [theme , setTheme] = React.useState(window.localStorage.getItem('theme') || 'light')
+  const [count , setCount] = React.useState(0)
 
-  function tasksFiltered(){
-    const storedTasks = JSON.parse(localStorage.getItem('Tasks'))
-    if(storedTasks){
-      return storedTasks
-    }
-  }
+//  React.useEffect(() => {
+//     window.localStorage.setItem('count', count);
+//   }, [count]);
 
   React.useEffect(()=>{
-    localStorage.setItem('Tasks' , JSON.stringify(filteredTasks))
-  } , [doList])
+    const storedData = JSON.parse(window.localStorage.getItem('Tasks'))
+    if(storedData){
+      setFilteredTasks(storedData)
+    }
+  } , [])
+  // React.useEffect(()=>{
+  //   localStorage.setItem('Tasks' , JSON.stringify(doList) )
+  // }, [doList])
+
+  //add the local storage in the final adding selection and then checked for the remaining
 
   const themeToggle = ()=>{
        window.localStorage.setItem('theme' , theme === 'light' ? 'dark' : 'light')
@@ -43,22 +48,29 @@ function App() {
               }
               setDoList(prev => [...prev, tasks])
               setFilteredTasks(prev => [...prev , tasks])
+              window.localStorage.setItem('Tasks' , JSON.stringify([...filteredTasks, tasks]))
               e.target.value = ''
       }
       }else{
           return
       }
   }
+
+  // function sortTasks(filteredArray){
+  //   const sortedTasks = filteredArray.sort((a, b) => Number(a.completed) - Number(b.completed));
+  // }
   React.useEffect(()=>{
-    let counter = 0
-    doList.map((t)=> {
+    let counter = 0;
+    filteredTasks.map((t)=> {
       t.completed ? counter = counter + 1 : counter
     })
     setCount(counter)
-  })
+  }, [filteredTasks])
 
+
+  
   function onChange(event){
-    const checkedBox = doList.map((tsks) => {
+    const checkedBox = JSON.parse(localStorage.getItem('Tasks')).map((tsks) => {
       if(event.target.id === tsks.id){
         return {
           ...tsks,
@@ -68,23 +80,47 @@ function App() {
       return tsks
     })
     setDoList(checkedBox)
+    setFilteredTasks(checkedBox)
+    // JSON.stringify(localStorage.setItem('Tasks' , [...JSON.parse(localStorage.getItem('Tasks')),  checkedBox]))
+    // JSON.stringify(localStorage.setItem('Tasks' , checkedBox))
+    window.localStorage.setItem('Tasks' , JSON.stringify(checkedBox))
   }
   function deleteHandling(id){
-    setDoList(doList.filter((detletedTasks) => detletedTasks.id != id ))
-    setFilteredTasks(doList.filter((detletedTasks) => detletedTasks.id != id ))
+    const lS = JSON.parse(localStorage.getItem("Tasks"));
+    const indexOfObject = lS.findIndex(object => {
+      return object.id === id;
+  });
+  lS.splice(indexOfObject, 1);
+  localStorage.setItem('Tasks', JSON.stringify(lS));
+  setFilteredTasks(lS)
   }
   function completedTasks(){
-    setDoList(doList.filter((detletedTasks) => !detletedTasks.completed))
-    setFilteredTasks(doList.filter((detletedTasks) => !detletedTasks.completed ))
+    // let index;
+    // const lS = JSON.parse(localStorage.getItem("Tasks"));
+    // for(let i = 0 ; i < lS.length ; i++){
+    //   if(lS[i].completed){
+    //     index = i;
+    //     break;
+    //   }
+    // }
+    // setDoList(doList.filter((detletedTasks) => !detletedTasks.completed))
+    // setFilteredTasks(doList.filter((detletedTasks) => !detletedTasks.completed ))
+    localStorage.setItem('Tasks' , JSON.stringify(filteredTasks.filter((deletedTasks) => !deletedTasks.completed)))
+    setFilteredTasks(filteredTasks.filter((deletedTasks) => !deletedTasks.completed))
+
   }
+  
   function taskFilter(event){
     if(event.target.textContent == 'All'){
-      setFilteredTasks(doList)
+      const allItems = JSON.parse(localStorage.getItem('Tasks'))
+
+      setFilteredTasks(allItems)
     }else if (event.target.textContent == "Active"){
-      const activeTasks = doList.filter(tsk => !tsk.completed)
+      const activeTasks = JSON.parse(localStorage.getItem('Tasks')).filter(tsk => !tsk.completed)
+
       setFilteredTasks(activeTasks)
     }else if(event.target.textContent == 'Completed'){
-      const completedTasks = doList.filter(tsk => tsk.completed)
+      const completedTasks = JSON.parse(localStorage.getItem('Tasks')).filter(tsk => tsk.completed)
       setFilteredTasks(completedTasks)
     }
   }
@@ -99,7 +135,7 @@ function App() {
     <ThemeContext.Provider value={{theme , themeToggle}}>
       <div id={theme}>
         <Header /> 
-        <Main theme = {theme} toggleTheme = {themeToggle} completion = {completedTasks} keyHandler= {keyHandler} counter = {doList.length - count} sort = {taskFilter}>
+        <Main theme = {theme} toggleTheme = {themeToggle} completion = {completedTasks} keyHandler= {keyHandler} counter = {filteredTasks.length - count} sort = {taskFilter}>
           {allTasks}
         </Main>
       </div>
